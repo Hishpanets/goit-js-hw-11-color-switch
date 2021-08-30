8b2c6098101ed75f142d1d6c68ef4c7c082370f6
Oleg Trukhachov <trukhachov91@gmail.com>
2021-08-30 16:44:18 +0200

    готово

---
+++ script.js
@@ -0,0 +1,44 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    start: document.querySelector("[data-action='start']"),
+    stop: document.querySelector("[data-action='stop']"),
+    body: document.querySelector("body")
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let isActive = false;
+let interval;
+
+function onStartButton() {
+    if (isActive) {
+        return;
+    }
+
+    isActive = true;
+    refs.start.disabled = true;
+    interval = setInterval(() => {
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+    }, 1000);
+}
+
+function onStopButton() {
+    isActive = false;
+    refs.start.disabled = false;
+    clearInterval(interval)
+}
+
+refs.start.addEventListener("click", onStartButton)
+
+refs.stop.addEventListener("click", onStopButton)
+
